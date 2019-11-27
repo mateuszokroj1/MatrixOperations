b967888395cf10794943eb25cf3faea83567de4c
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-11-28 00:04:04 +0100

    Added indexer to matrix

--- LinearEquationsSolver/MatrixOperations/Matrix.cs
+++ LinearEquationsSolver/MatrixOperations/Matrix.cs
@@ -2,9 +2,28 @@
 
 namespace MatrixOperations
 {
-    public class Matrix<Tsource>
+    public class Matrix<Tsource> where Tsource : struct
     {
-        protected Tsource[][] value = new Tsource[0][];
+        protected Tsource[][] value;
 
+        public Matrix(Tsource[][] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+
+            value = array;
+        }
+
+        #region Properties
+
+        public bool IsSquare => this.value.GetUpperBound(0) == this.value.GetUpperBound(1);
+
+        public Tsource this[int index1, int index2]
+        {
+            get => this.value[index1][index2];
+            set { this.value[index1][index2] = value; }
+        }
+
+        #endregion
     }
 }
