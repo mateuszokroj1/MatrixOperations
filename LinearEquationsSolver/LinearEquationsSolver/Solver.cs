58febebeb58b2d74c38d880c4c5706b6c3f4740c
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-11-27 23:06:05 +0100

    Initial commit

---
+++ LinearEquationsSolver/LinearEquationsSolver/Solver.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LinearEquationsSolver
+{
+    public static class Solver
+    {
+    }
+}
---
+++ LinearEquationsSolver/MatrixOperations/Matrix.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MatrixOperations
+{
+    public class Matrix<Tsource>
+    {
+        protected Tsource[][] value = new Tsource[0][];
+
+    }
+}
