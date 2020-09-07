d0924297a5c871e9655f04f3131b4d0635e21f60
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-07 17:16:45 +0200

    Indexer tests

--- LinearEquationsSolver/Tests/MatrixOperations.Tests/Collections/RowCollectionTest.cs
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/Collections/RowCollectionTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -10,7 +11,55 @@ namespace MatrixOperations.Tests.Collections
         #region Constructors
 
         [Fact]
-        public void Constructor1_WhenArgumentIsNull_ShouldThrowArgumentIsNull()
+        public void Constructor_WhenArgumentIsNull_ShouldThrowArgumentIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new RowCollection<int>(null));
+        }
+
+        [Fact]
+        public void Constructor_WhenArgumentIsEmpty_ShouldBeInitialized()
+        {
+            var collection = new RowCollection<float>(new Matrix<float>());
+            Assert.NotNull(collection);
+            Assert.Empty(collection);
+        }
+
+        [Fact]
+        public void Constructor_WhenArgumentIsNotEmpty_ShouldHaveEqualItems()
+        {
+            var array = new short[][]
+            {
+                new short[] { 2, 3, 1 },
+                new short[] { -2, 0, 10 }
+            };
+            var matrix = new Matrix<short>(array);
+            var collection = new RowCollection<short>(matrix);
+
+            Assert.Equal(2, collection.Count);
+
+            for (byte row = 0; row < 2; row++)
+            {
+                Assert.NotNull(collection.ElementAt(row));
+                for (byte column = 0; column < 3; column++)
+                    Assert.Equal(array[row][column], collection.ElementAt(row)[column]);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Fact]
+        public void Indexer_Get_ShouldReturnRow()
+        {
+            var matrix = new Matrix<double>(4,4);
+            var collection = new RowCollection<double>(matrix);
+            for (byte row = 0; row < 4; row++)
+                Assert.Equal(matrix.value[row], collection[row]);
+        }
+
+        [Fact]
+        public void Indexer_Set_WhenArgumentIsOutOfRange_ShouldThrowIndexOutOfRange()
         {
 
         }
