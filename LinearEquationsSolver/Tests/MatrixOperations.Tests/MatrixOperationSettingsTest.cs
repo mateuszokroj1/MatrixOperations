540d0d329878e89aa8c018b12ccf2afeffbfc9a2
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-07 10:38:40 +0200

    Created test for MatrixOperationSettings

---
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/MatrixOperationSettingsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xunit;
+
+namespace MatrixOperations.Tests
+{
+    public class MatrixOperationSettingsTest
+    {
+        [Fact]
+        public void MinimumCountForParallelMode_ShouldSetAndGet()
+        {
+            MatrixOperationsSettings.MinimumCountForParallelMode = 3;
+            Assert.Equal(3, MatrixOperationsSettings.MinimumCountForParallelMode);
+        }
+
+        [Fact]
+        public void CanRunInParallelMode_ShouldHaveDefaultValueValid()
+        {
+            Assert.Equal(Environment.ProcessorCount > 2, MatrixOperationsSettings.CanRunInParallelMode);
+        }
+
+        [Fact]
+        public void CanRunInParallelMode_ShouldSetAndGetValue()
+        {
+            MatrixOperationsSettings.CanRunInParallelMode = false;
+            Assert.False(MatrixOperationsSettings.CanRunInParallelMode);
+
+            MatrixOperationsSettings.CanRunInParallelMode = true;
+            Assert.True(MatrixOperationsSettings.CanRunInParallelMode);
+        }
+    }
+}
