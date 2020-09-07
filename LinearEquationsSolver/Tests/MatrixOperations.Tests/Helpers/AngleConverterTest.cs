8b73160a385c3f8a07f3729075550eceb3630730
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-07 11:05:35 +0200

    Created test for AngleConverter class.

---
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/Helpers/AngleConverterTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xunit;
+
+namespace MatrixOperations.Tests.Helpers
+{
+    public class AngleConverterTest
+    {
+        [Fact]
+        public void ConvertDegreesToRadians_ShouldReturnValidValue()
+        {
+
+        }
+    }
+}
