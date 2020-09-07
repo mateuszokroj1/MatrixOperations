8ed7ae00221351e1a1ae25483c5af3e12f439d9a
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-07 11:43:42 +0200

    Created test for RowCollection class.

---
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/Collections/RowCollectionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MatrixOperations.Tests.Collections
+{
+    public class RowCollectionTest
+    {
+        #region Constructors
+
+        [Fact]
+        public void Constructor1_WhenArgumentIsNull_ShouldThrowArgumentIsNull()
+        {
+
+        }
+
+        #endregion
+    }
+}
