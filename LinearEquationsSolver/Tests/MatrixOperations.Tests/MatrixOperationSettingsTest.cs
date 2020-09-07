c8103971017db825522b9e8e2202de881bb086a7
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-07 10:56:24 +0200

    Added test for CheckIsParallelModeUseful method

--- LinearEquationsSolver/Tests/MatrixOperations.Tests/MatrixOperationSettingsTest.cs
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/MatrixOperationSettingsTest.cs
@@ -28,5 +28,25 @@ namespace MatrixOperations.Tests
             MatrixOperationsSettings.CanRunInParallelMode = true;
             Assert.True(MatrixOperationsSettings.CanRunInParallelMode);
         }
+
+        [Fact]
+        public void CheckIsParallelModeUseful_ShouldReturnValidValue()
+        {
+            MatrixOperationsSettings.CanRunInParallelMode = false;
+            MatrixOperationsSettings.MinimumCountForParallelMode = 10;
+            Assert.False(MatrixOperationsSettings.CheckIsParallelModeUseful(20));
+
+            MatrixOperationsSettings.CanRunInParallelMode = true;
+            MatrixOperationsSettings.MinimumCountForParallelMode = 200;
+            Assert.True(MatrixOperationsSettings.CheckIsParallelModeUseful(200L));
+
+            MatrixOperationsSettings.CanRunInParallelMode = true;
+            MatrixOperationsSettings.MinimumCountForParallelMode = 2000;
+            Assert.False(MatrixOperationsSettings.CheckIsParallelModeUseful(100.0f));
+
+            MatrixOperationsSettings.CanRunInParallelMode = true;
+            MatrixOperationsSettings.MinimumCountForParallelMode = 2000;
+            Assert.False(MatrixOperationsSettings.CheckIsParallelModeUseful(5000M));
+        }
     }
 }
