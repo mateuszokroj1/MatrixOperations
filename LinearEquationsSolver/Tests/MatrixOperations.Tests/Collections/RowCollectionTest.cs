566c0d5e07990778e1b5e161613aa451e2930ac2
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-08 12:41:01 +0200

    Works with RowCollection test.

--- LinearEquationsSolver/Tests/MatrixOperations.Tests/Collections/RowCollectionTest.cs
+++ LinearEquationsSolver/Tests/MatrixOperations.Tests/Collections/RowCollectionTest.cs
@@ -59,7 +59,104 @@ namespace MatrixOperations.Tests.Collections
         }
 
         [Fact]
-        public void Indexer_Set_WhenArgumentIsOutOfRange_ShouldThrowIndexOutOfRange()
+        public void Indexer_GetSet_WhenArgumentIsOutOfRange_ShouldThrowIndexOutOfRange()
+        {
+            var matrix = new Matrix<double>(4, 4);
+            var collection = new RowCollection<double>(matrix);
+
+            Assert.Throws<IndexOutOfRangeException>(() => collection[4]);
+            Assert.Throws<IndexOutOfRangeException>(() => collection[-1]);
+            Assert.Throws<IndexOutOfRangeException>(() => collection[int.MaxValue] = new double[] { 2 });
+        }
+
+        [Fact]
+        public void Indexer_Set_WhenIndexIsValidAndRowIsInvalid_ShouldThrowInvalidOperationException()
+        {
+            var matrix = new Matrix<double>(4, 4);
+            var collection = new RowCollection<double>(matrix);
+
+            Assert.Throws<InvalidOperationException>(() => collection[1] = new double[] { 2 });
+        }
+
+        [Fact]
+        public void Indexer_Set_WhenArgumentsIsValid_ShouldSetValue()
+        {
+            var matrix = new Matrix<double>(4, 4);
+            var collection = new RowCollection<double>(matrix);
+
+            collection[0] = new double[] { 1, 2, 3, 4 };
+        }
+
+        [Fact]
+        public void Count_ShouldReturnArrayLength()
+        {
+            var matrix = new Matrix<double>(10, 4);
+            var collection = new RowCollection<double>(matrix);
+            Assert.Equal(matrix.value.Length, collection.Count);
+        }
+
+        [Fact]
+        public void LongCount_ShouldReturnArrayLength()
+        {
+            var matrix = new Matrix<double>(20, 20);
+            var collection = new RowCollection<double>(matrix);
+            Assert.Equal(matrix.value.LongLength, collection.LongCount);
+        }
+
+        [Fact]
+        public void IsReadOnly_ShouldReturnFalse()
+        {
+            var matrix = new Matrix<double>(20, 20);
+            var collection = new RowCollection<double>(matrix);
+            Assert.False(collection.IsReadOnly);
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void IndexOf_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+        {
+            var matrix = new Matrix<double>(20, 20);
+            var collection = new RowCollection<double>(matrix);
+
+            Assert.Throws<ArgumentNullException>(() => collection.IndexOf(null));
+
+            matrix = new Matrix<double>();
+            collection = new RowCollection<double>(matrix);
+            Assert.Throws<ArgumentNullException>(() => collection.IndexOf(null));
+        }
+
+        [Theory]
+        [InlineData(new double[] { 2, 3 })]
+        [InlineData(new double[] { 3, 4, 1, 0 })]
+        public void IndexOf_WhenRowNotExists_ShouldReturnMinus1(double[] rowsToTest)
+        {
+            var matrix = new Matrix<double>(20, 5);
+            var collection = new RowCollection<double>(matrix);
+
+            Assert.Equal(-1, collection.IndexOf(rowsToTest));
+        }
+
+        [Theory]
+        [InlineData(0, new int[] { 1, 2, 3, 4 })]
+        [InlineData(1, new int[] { 10, 9, 8, 7 })]
+        public void IndexOf_WhenRowExists_ShouldReturnFirstIndex(int expectedValue, int[] rowToTest)
+        {
+            var matrix = new Matrix<int>(new int[][]
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 10, 9, 8, 7 }
+            });
+
+            var collection = new RowCollection<int>(matrix);
+
+            Assert.Equal(expectedValue, collection.IndexOf(rowToTest));
+        }
+
+        [Fact]
+        public void Insert_WhenIndexIsOutOfRange_ShouldThrowIndexOutOfRangeException()
         {
 
         }
